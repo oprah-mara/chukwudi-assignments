05429b4b30b558cfd4fb8b8a1efc5638ffe943ec
https://github.com/oprah-mara <oparaamara997@gmail.com>
2024-04-24 13:33:24 -0400

    we added eventlistenter to the backIcon,sub-1 and fistlink on jscript 4/24/2024

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 let hanburger = document.getElementById("hanburger")
 let navGroup = document.getElementById("nav_link_group")
+let firstlink = document.getElementById("firstlink")
+let sub1= document.getElementById("sub-1")
+let backIcon = document.getElementById("backIcon")
+
+firstlink.addEventListener("click",function(){
+    navGroup.style.left = "-100%"
+    sub1.style.left = "0"  
+})
+backIcon.addEventListener("click",function(){
+    sub1.style.left = "100%"
+    navGroup.style.left = "0"
+}
+)
+
 
 hanburger.addEventListener("click", function()
 {
@@ -9,8 +23,6 @@ hanburger.addEventListener("click", function()
     } else{
         navGroup.style.visibility = "visible";
          navGroup.style.opacity = "1";
-
-
         }
       
     }
